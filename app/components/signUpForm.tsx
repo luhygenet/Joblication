87d888b860c1c46def5f0bbb127c1a38860aec27
Liya Daniel <luhygenet@gmail.com>
2025-11-17 22:31:31 +0300

    started react hook and added visualization

--- app/components/signUpForm.tsx
+++ app/components/signUpForm.tsx
@@ -1,7 +1,12 @@
+"use client"
 import React from "react";
 import GeneralButton from "./generalButton";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const SignUpForm = () => {
+  const form = useForm();
+  const { register, control } = form;
   return (
     <>
       <form
@@ -15,29 +20,33 @@ const SignUpForm = () => {
           type="text"
           id="fullname"
           className=" py-2 border border-[#CCCCF5] rounded text-[#4640DE]"
+          {...register("fullname")}
         />
 
         <label htmlFor="email">Email Address</label>
 
         <input
-          type="text"
+          type="email"
           id="email"
           className=" py-2 border border-[#CCCCF5] rounded text-[#4640DE]"
+          {...register("email")}
         />
         <label htmlFor="password">Password</label>
 
         <input
-          type="text"
+          type="password"
           id="password"
           className=" py-2 border border-[#CCCCF5] rounded text-[#4640DE]"
+          {...register("password")}
         />
 
         <label htmlFor="Confirm password">Confirm Password</label>
 
         <input
-          type="text"
+          type="password"
           id="Confirm password"
           className=" py-2 border mb-2 border-[#CCCCF5] rounded text-[#4640DE]"
+          {...register("confirmPassword")}
         />
         <GeneralButton
           font="Epilogue"
@@ -45,7 +54,6 @@ const SignUpForm = () => {
           fontWeight={700}
           textColor="text-white"
           w={700}
-      
           border={1}
           borderColor="[#CCCCF5]"
           rounded="rounded-4xl"
@@ -56,8 +64,8 @@ const SignUpForm = () => {
           py={2}
           text="Continue"
         />
-        
       </form>
+      <DevTool control={control} />
     </>
   );
 };

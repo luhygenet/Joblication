fa7baa824200130cd0a0fadc5d16b7967064bd16
Liya Daniel <luhygenet@gmail.com>
2026-02-14 19:51:02 +0300

    eslint when deploying

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Warning: ignore ESLint errors during production builds
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
 export default nextConfig;
